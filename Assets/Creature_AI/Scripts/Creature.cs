efa8ed85cb250c5a038f6c9bf58bc37b26c331c3
mandu <jys0972@gmail.com>
2024-07-30 17:18:45 +0900

    Create(Creature.cs

---
+++ Assets/Creature_AI/Scripts/Creature.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : Actor
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
