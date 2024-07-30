57d34d066de46e8f7b608eda4d368603186d4ef8
191012 <kimhj191012@naver.com>
2024-07-30 17:02:55 +0900

    Set character stop direction

--- Assets/KHJ/Scripts/CharacterMove.cs
+++ Assets/KHJ/Scripts/CharacterMove.cs
@@ -17,6 +17,12 @@ public class CharacterMove : MonoBehaviour
     {
         playerRb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * playerMoveSpeed * Time.deltaTime;
 
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            Anim.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
+            Anim.SetFloat("LastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+        
         Anim.SetFloat("MoveX", playerRb.velocity.x);
         Anim.SetFloat("MoveY", playerRb.velocity.y);
     }
