724a2aab43eccdd1dddbb08143afd993afb1dfea
mandu <jys0972@gmail.com>
2024-08-04 15:51:14 +0900

    feat(Creature.cs) : 지속적으로 플레이어를 쫒기 위한 로직 구현

--- Assets/Creature_AI/Scripts/Creature.cs
+++ Assets/Creature_AI/Scripts/Creature.cs
@@ -5,42 +5,65 @@ using UnityEngine;
 
 public class Creature : Actor
 {
-   int[,] grid = new int[,]
-   {
-        { 0, 1, 0, 0, 0, 0, 0 },
-        { 0, 1, 0, 1, 1, 1, 0 },
-        { 0, 1, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 1, 0, 1, 0 },
-        { 0, 1, 0, 1, 0, 1, 0 },
-        { 0, 1, 0, 0, 0, 0, 0 },
-        { 0, 1, 0, 1, 1, 1, 0 },
-        { 0, 0, 0, 0, 0, 0, 0 }
-   };
+    [SerializeField] Player player;
+    int[,] grid = new int[,]
+    {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 1, 0, 0, 0, 0, 0 ,1},
+        {1, 0, 1, 0, 1, 1, 1, 0 ,1},
+        {1, 0, 1, 0, 0, 0, 0, 0 ,1},
+        {1, 0, 0, 0, 1, 0, 1, 0 ,1},
+        {1, 0, 1, 0, 1, 0, 1, 0 ,1},
+        {1, 0, 1, 0, 0, 0, 0, 0 ,1},
+        {1, 0, 1, 0, 1, 1, 1, 0 ,1},
+        {1, 0, 0, 0, 0, 0, 0, 0 ,1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
 
-    Node startNode = new Node(0, 0, true);
-    Node endNode = new Node(6, 6, true);
+    Node startNode = new Node(1, 1, true);
+    Node endNode = new Node(7, 7, true);
 
     void Start()
     {
+        StartCoroutine(MoveToP());
+    }
+
+    IEnumerator MoveToP()
+    {
+        yield return new WaitForSeconds(5);
+        temp();  
+    }
+    void temp()
+    {
+        StopAllCoroutines();
+        MoveToPlayer();
+        StartCoroutine(MoveToP());
+    }
+    
+
+    void MoveToPlayer()
+    {
+        startNode = new Node((int)transform.position.x+8, (int)transform.position.y+4, true);
+        endNode = new Node((int)player.transform.position.x + 8, (int)player.transform.position.y+4, true);
         PathFinder pathFinder = new PathFinder(grid);
         try
         {
             List<Node> path = pathFinder.FindPath(startNode, endNode);
-            print(path);
-            foreach (Node node in path)
-            {
-                Console.WriteLine($"X: {node.X}, Y: {node.Y}");
-            }
+            StartCoroutine(MoveOnPath(path));
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             print(e.Message);
         }
     }
 
     
-    void Update()
+    IEnumerator MoveOnPath(List<Node> path)
     {
-        
+        foreach(Node node in path)
+        {
+            yield return new WaitForSeconds(1f);
+            transform.position = new Vector3(node.X - 8, node.Y-4);
+        }
     }
 }
