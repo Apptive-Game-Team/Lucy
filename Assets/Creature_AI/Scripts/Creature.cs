928510cb356e158d3ab38865dbd79a0791df8e4b
mandu <jys0972@gmail.com>
2024-08-04 13:36:49 +0900

    feat(Creature.cs) : test 용 코드 추가

--- Assets/Creature_AI/Scripts/Creature.cs
+++ Assets/Creature_AI/Scripts/Creature.cs
@@ -1,13 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Creature : Actor
 {
-    
+   int[,] grid = new int[,]
+   {
+        { 0, 1, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 1, 1, 1, 0 },
+        { 0, 1, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 1, 0, 1, 0 },
+        { 0, 1, 0, 1, 0, 1, 0 },
+        { 0, 1, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 1, 1, 1, 0 },
+        { 0, 0, 0, 0, 0, 0, 0 }
+   };
+
+    Node startNode = new Node(0, 0, true);
+    Node endNode = new Node(6, 6, true);
+
     void Start()
     {
-        
+        PathFinder pathFinder = new PathFinder(grid);
+        try
+        {
+            List<Node> path = pathFinder.FindPath(startNode, endNode);
+            print(path);
+            foreach (Node node in path)
+            {
+                Console.WriteLine($"X: {node.X}, Y: {node.Y}");
+            }
+        }
+        catch(Exception e)
+        {
+            print(e.Message);
+        }
     }
 
     
