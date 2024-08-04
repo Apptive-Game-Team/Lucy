96c979c20414e42a90658b883a407beba255f6f3
mandu <jys0972@gmail.com>
2024-08-04 22:43:04 +0900

    feat(MapBuilder.cs) : Manager 참조)

--- Assets/Creature_AI/Scripts/MapBuilder.cs
+++ Assets/Creature_AI/Scripts/MapBuilder.cs
@@ -6,28 +6,17 @@ public class MapBuilder : MonoBehaviour
 {
 
     [SerializeField] GameObject wall;
-    int[,] grid = new int[,]
-    {
-        {1, 1, 1, 1, 1, 1,1, 1, 1},
-        { 1, 0, 1, 0, 0, 0, 0, 0 ,1},
-        {1,  0, 1, 0, 1, 1, 1, 0 ,1},
-        {1,  0, 1, 0, 0, 0, 0, 0 ,1},
-        {1,  0, 0, 0, 1, 0, 1, 0 ,1},
-        {1,  0, 1, 0, 1, 0, 1, 0 ,1 },
-        {1,  0, 1, 0, 0, 0, 0, 0 ,1},
-        {1, 0, 1, 0, 1, 1, 1, 0 ,1},
-        {1, 0, 0, 0, 0, 0, 0, 0 ,1},
-        {1, 1, 1, 1, 1, 1,1, 1, 1}
-    };
+    
 
-    // Start is called before the first frame update
+    
     void Start()
     {
-        for (int i = 0; i<grid.GetLength(0); i++)
+        int[,] map = CreatureManager.Instance.GetMap();
+        for (int i = 0; i< map.GetLength(0); i++)
         {
-            for (int j = 0; j<grid.GetLength(1); j++)
+            for (int j = 0; j< map.GetLength(1); j++)
             {
-                if (grid[i, j] == 1)
+                if (map[i, j] == 1)
                 {
                     MakeWall(i, j);
                 }
@@ -38,6 +27,6 @@ public class MapBuilder : MonoBehaviour
 
     void MakeWall(int x, int y)
     {
-        Instantiate(wall, new Vector3(x-8, y-4), Quaternion.identity);
+        Instantiate(wall, new Vector3(x, y), Quaternion.identity);
     }
 }
