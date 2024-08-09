73993e9ae2c5b63e95e8714bbc8933dbdff15bcf
mandu <jys0972@gmail.com>
2024-08-09 23:01:57 +0900

    feat(Creature.cs) : namespace 추가, 움직임 코루틴 하나로 통일, Detecter 연결, Action class로 관리

--- Assets/Creature_AI/Scripts/Creature.cs
+++ Assets/Creature_AI/Scripts/Creature.cs
@@ -2,76 +2,233 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class Creature : Actor
-{
-    PathLineRenderer pathLineRenderer;
-    Player player;
-    int[,] map;
-    Vector3 deltaPosition = new Vector3();
-    Node startNode = new Node(true);
-    Node endNode = new Node(true);
-    float moveFrame = 0.05f;
-
-    void Start()
+namespace Creature{
+    public enum CreatureStatus
     {
-        player = CreatureManager.Instance.GetPlayer();
-        map = CreatureManager.Instance.GetMap();
-        pathLineRenderer = GetComponent<PathLineRenderer>();
-        StartCoroutine(Schedule());
+        PATROL = 0, // 기본
+        PURSUIT = 1, // 추적
+        ALERTED = 2, // 의심
     }
 
-    IEnumerator Schedule()
+    public class CreatureAction
     {
-        yield return new WaitForSeconds(5);
-        temp();  
+        protected Creature creature;
+        public virtual void Play() {}
     }
 
-    void temp()
+    public class CreaturePatrolAction : CreatureAction
     {
-        StopAllCoroutines();
-        List<Node> path = FindPath(player.transform.position.x, player.transform.position.y);
-        if (path != null)
+        public CreaturePatrolAction(Creature creature)
         {
-            StartCoroutine(MoveOnPath(path));
+            this.creature = creature;
         }
-        StartCoroutine(Schedule());
-    }
-    
-    private List<Node> FindPath(float x, float y)
-    {
-        startNode.SetPosition(transform.position.x, transform.position.y);
-        endNode.SetPosition(x, y);
-        PathFinder pathFinder = new PathFinder(map);
-        try
+
+        public override void Play()
         {
-            List<Node> path = pathFinder.FindPath(startNode, endNode);
-            pathLineRenderer.SetPoints(path);
-            return path;
-        }
-        catch (Exception e)
-        {
-            print(e.Message);
-            return null;
+            creature.StartCoroutine(creature.PatrolAction());
         }
     }
 
-    
-    IEnumerator MoveOnPath(List<Node> path)
+    public class CreaturePursuitAction : CreatureAction
     {
-        foreach(Node node in path)
+        public CreaturePursuitAction(Creature creature)
         {
-            float deltaX = node.X - transform.position.x;
-            float deltaY = node.Y - transform.position.y;
-            deltaPosition.Set(deltaX, deltaY, 0);
-            deltaPosition.Normalize();
+            this.creature = creature;
+        }
+
+        public override void Play()
+        {
+            creature.StartCoroutine(creature.PursuitAction());
+        }
+    }
+
+    public class CreatureAlertedAction : CreatureAction
+    {
+        public CreatureAlertedAction(Creature creature)
+        {
+            this.creature = creature;
+        }
+
+        public override void Play()
+        {
+            creature.StartCoroutine(creature.AlertedAction());
+        }
+    }
+
+    public class Creature : Actor
+    {
+        [SerializeField] bool debugMode = true;
+
+        Detector detector;
+
+        PathLineRenderer pathLineRenderer;
+        int[,] map;
+        Vector3Int mapOffset;
+        Vector3 deltaPosition = new Vector3();
+        Node startNode = new Node(true);
+        Node endNode = new Node(true);
+        float moveFrame = 0.05f;
+
+        protected Vector3 targetPosition;
+        List<Node> path;
+
+        bool isChasing = false;
+
+        protected List<CreatureAction> actions;
+
+        protected CreatureStatus status;
+        Coroutine alertedCounterCoroutine;
             
-            while (Math.Abs(deltaX) > 0.1 && Math.Abs(deltaY) > 0.1){
-                transform.position += (deltaPosition * speed * moveFrame);
-                yield return new WaitForSeconds(moveFrame);
+        private void InitActions()
+        {
+            actions = new List<CreatureAction>()
+            {
+                new CreaturePatrolAction(this),
+                new CreaturePursuitAction(this),
+                new CreatureAlertedAction(this)
+            };
+
+        }
+
+        protected void Start()
+        {
+            status = CreatureStatus.PATROL;
+            map = CreatureManager.Instance.GetMap();
+            mapOffset = CreatureManager.Instance.GetMapOffset();
+            pathLineRenderer = GetComponent<PathLineRenderer>();
+            detector = GetComponent<Detector>();
+            detector.SetTargetMask(LayerMask.GetMask("Player"));
+            InitActions();
+            actions[(int)status].Play();
+            StartCoroutine(MoveOnPath()); 
+        }
+
+        public virtual IEnumerator PatrolAction()
+        {
+            DetectPlayer();
+            yield return new WaitForSeconds(0.1f);
+            actions[(int)status].Play();
+        }
+
+        public virtual IEnumerator PursuitAction()
+        {
+            DetectPlayer();
+            yield return new WaitForSeconds(0.1f);
+            GetPathToPosition(targetPosition);
+            actions[(int)status].Play();
+        }
+
+        public virtual IEnumerator AlertedAction()
+        {
+            DetectPlayer();
+            detector.setLookingAngle(detector.getLookingAngle() + 10f);
+            yield return new WaitForSeconds(0.1f);
+            actions[(int)status].Play();
+        }
+
+        public IEnumerator AlertedCounter()
+        {
+            yield return new WaitForSeconds(10f);
+            if (status.Equals(CreatureStatus.ALERTED))
+                status = CreatureStatus.PATROL;
+            alertedCounterCoroutine = null;
+        }
+
+        protected void DetectPlayer()
+        {
+            List<Collider2D> detectedPlayerCollider = detector.DetectByView();
+            if (detectedPlayerCollider.Count > 0)
+            {
+                status = CreatureStatus.PURSUIT;
+                Vector3 detectedPlayerPosition = detectedPlayerCollider[0].transform.position;
+                targetPosition = detectedPlayerPosition;
+            } else if (status.Equals(CreatureStatus.PURSUIT) && !isChasing)
+            { 
+                status = CreatureStatus.ALERTED;
+                if (alertedCounterCoroutine != null)
+                {
+                    StopCoroutine(alertedCounterCoroutine);
+                    alertedCounterCoroutine = null;
+                }
+                alertedCounterCoroutine = StartCoroutine(AlertedCounter());
             }
-            transform.position = new Vector3(node.X, node.Y);
+        }
+
+        protected void GetPathToPosition(Vector3 targetPosition)
+        {
+            path = FindPath(targetPosition.x, targetPosition.y);
+        }
+
+
+        protected List<Node> FindPath(float x, float y)
+        {
+            startNode.SetPosition(transform.position.x, transform.position.y);
+            endNode.SetPosition(x, y);
+            PathFinder pathFinder = new PathFinder(map, mapOffset);
+            try
+            {
+                List<Node> path = pathFinder.FindPath(startNode, endNode);
+                if (debugMode)
+                {
+                    pathLineRenderer.SetPoints(path);
+                }
+                return path;
+            }
+            catch (Exception e)
+            {
+                print(e.Message);
+                return null;
+            }
+        }
+
+
+        IEnumerator MoveOnPath()
+        {
+            while(true)
+            {
+
+                yield return new WaitForSeconds(0.5f);
+
+                Node node;
+                try
+                {
+                    node = path[0];
+                    if (node.X == transform.position.x && node.Y == transform.position.y)
+                    {
+                        
+                        node = path[1];
+                        path.RemoveAt(1);
+                    }
+                    path.RemoveAt(0);
+                }
+                catch
+                {
+                    isChasing = false;
+                    continue;
+                }
+
+                isChasing = true;
+
+                float deltaX = node.X - transform.position.x;
+                float deltaY = node.Y - transform.position.y;
+                deltaPosition.Set(deltaX, deltaY, 0);
+
+                detector.SetLookingDirection(deltaPosition);
+
+                deltaPosition.Normalize();
+
+                //while (Math.Abs(deltaX) > 0.1 && Math.Abs(deltaY) > 0.1){
+                //    transform.position += (deltaPosition * speed * moveFrame);
+                //    yield return new WaitForSeconds(moveFrame);
+                //}
+                yield return new WaitForSeconds(0.5f);
+                transform.position = new Vector3(node.X, node.Y);
+            }
+            
         }
     }
 }
+
