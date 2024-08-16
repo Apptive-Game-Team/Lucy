11ad0774e328e803c0259633114e761c238dbe2c
hwanginseop <163392234+hwanginseop@users.noreply.github.com>
2024-08-16 16:51:44 +0900

    캐릭터 스탯 생성

---
+++ Assets/HIS/Script/Character_Stat.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Stat : MonoBehaviour
+{
+    [Header("Player Stat")]
+    public int Mental = 100;
+    public int Stamina = 100;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
