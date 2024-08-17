934f3c5a0c0628b48f37a0b9454c8ef3a9ae30e7
mandu <jys0972@gmail.com>
2024-08-17 17:23:54 +0900

    feat(HandLight.cs) : 손전등 로직 추가

---
+++ Assets/Creature_AI/Scripts/HandLight.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Creature
+{
+    public class HandLight : MonoBehaviour
+    {
+        Detector detector;
+        Avoider avoider;
+        void Start()
+        {
+            detector = gameObject.GetComponent<Detector>();
+            detector.SetTargetMask(LayerMask.GetMask("Creature"));
+        }
+
+        void Update()
+        {
+            List<Collider2D> targets =  detector.DetectByView();
+            foreach (Collider2D target in targets)
+            {
+                try
+                {
+                    avoider = target.GetComponent<Avoider>();
+                }
+                catch
+                {
+                    continue;
+                }
+                avoider.OnDetectedByHandLight(transform.position);
+            }
+        }
+    }
+}
+
