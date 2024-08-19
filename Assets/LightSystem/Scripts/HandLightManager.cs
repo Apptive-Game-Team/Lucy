98b3ff3f7c6af9898a1d3573f8c9c34bb1f09973
Monolong <83206119+Monolong@users.noreply.github.com>
2024-08-19 17:28:32 +0900

    feat(Door, Light Rotation) (#42)
    * feat(Door, Light Rotation)
    
    문 시스템 일단 추가했습니다. 아직 열쇠 로 문 따는 기능은 미구현(인벤토리쪽 좀 더 봐야할 듯)
    그리고 손전등 돌아가는 것 구현 했습니다.
    
    * 씬 충돌 해결을 위한 프리팹 생성
    
    씬 충돌 해결을 위한 프리팹 생성

---
+++ Assets/LightSystem/Scripts/HandLightManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandLightManager : MonoBehaviour
+{
+    [SerializeField] HandLightRotator hlRotator;
+    [SerializeField] HandLightSwitch hlSwitch;
+}
