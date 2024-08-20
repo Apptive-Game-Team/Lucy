d17dc523e440a7ff2e1f5de6f8be89eb59c5b581
mandu <jys0972@gmail.com>
2024-08-20 17:25:41 +0900

    feat(PasswordSystem.cs) : 비밀번호 UI 관리 class 구현

---
+++ Assets/PasswordSystem/Scripts/PasswordSystem.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PasswordSystem : MonoBehaviour
+{
+    [SerializeField] private string currentPassword = "";
+
+    private string password = "";
+
+    PasswordObject passwordObject;
+
+    Canvas passwordCanvas;
+
+    TMP_Text text;
+
+    [SerializeField] List<Button> buttons;
+
+    private void Awake()
+    {
+        text = gameObject.GetComponentInChildren<TMP_Text>();
+        passwordCanvas = gameObject.GetComponentInChildren<Canvas>();
+    }
+
+    void Start()
+    {
+        InitButtonsOnClick();
+        passwordCanvas.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        currentPassword = "";
+        text.SetText(currentPassword);
+    }
+
+    public void SetPassword(string password, PasswordObject passwordObject)
+    {
+        this.passwordObject = passwordObject;
+        passwordCanvas.gameObject.SetActive(true);
+        this.password = password;
+    }
+
+    private void InitButtonsOnClick()
+    {
+        for (int i = 0; i < 10; i++) {
+            int index = i;
+            buttons[i].onClick.AddListener(() =>
+            {
+                currentPassword += index.ToString();
+                text.SetText(currentPassword);
+                
+            });
+        }
+
+        buttons[10].onClick.AddListener(() =>
+        {
+            currentPassword = "";
+            text.SetText(currentPassword);
+        });
+
+        buttons[11].onClick.AddListener(() =>
+        {
+            if (password == currentPassword)
+            {
+                passwordObject.Unlock();
+                passwordCanvas.gameObject.SetActive(false);
+            } else
+            {
+                currentPassword = "";
+                text.SetText(currentPassword);
+            }
+        });
+    }
+}
