dcd91ba9c507bc5fcb439d4ef96242b08e002291
Gimlocal <127363458+Gimlocal@users.noreply.github.com>
2024-08-26 12:40:18 +0900

    67 달리기 및 대사상속화 (#69)
    * feat(Character Run)
    
    캐릭터 달리기 추가 - 달릴때 애니메이션 배속
    
    몬스터와 속도 맞추는 작업 추가요망
    
    * feat(Add Test Scene of Dialogue)
    
    테스트용 대사 스크립트 생성 & 테스트 씬
    
    * feat(Change DialogueData to ScriptableObject)
    
    대사 데이터를 스크립터블 오브젝트로 변경

---
+++ Assets/JJW/Scripts/TestDialogueController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class TestDialogueController : MonoBehaviour
+    {
+        public TestDialogueData testDialogueData;
+        public int GetOutFirstRoom = 0;
+        public int GetInRightDownRoom = 0;
+        public int GetInRightUpRoom = 0;
+        public int CheckStairRoom = 0;
+
+        public void SetDialogueFlag(int num)
+        {
+            switch (num)
+            {
+                case 0:
+                    GetOutFirstRoom++;
+                    break;
+                case 1:
+                    GetInRightDownRoom++;
+                    break;
+                case 2:
+                    GetInRightUpRoom++;
+                    break;
+                case 3:
+                    CheckStairRoom++;
+                    break;
+            }
+        }
+
+        void Start()
+        {
+            StartCoroutine(StartDialogue()); // 처음 시작할때 스크립트 재생
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TestDialogueSystem.Instance.ShowDialogue(testDialogueData.GetDialogue("Chapter1","FirstRoom")[1]);
+            }
+
+            if (GetOutFirstRoom == 1 || Input.GetKeyDown(KeyCode.Alpha1)) // 문열고 복도로 나갈때 
+            {
+                TestDialogueSystem.Instance.ShowDialogue(testDialogueData.GetDialogue("Chapter1","Corrider")[0]);
+                GetOutFirstRoom++;
+            }
+
+            if (GetInRightDownRoom == 1 || Input.GetKeyDown(KeyCode.Alpha2)) // 오른쪽 방 들어갔을때
+            {
+                TestDialogueSystem.Instance.ShowDialogue(testDialogueData.GetDialogue("Chapter1","RightRoom")[0]);
+                GetInRightDownRoom++;
+            }
+
+            if (GetInRightUpRoom == 1 || Input.GetKeyDown(KeyCode.Alpha3)) // 오른쪽 방 위에 들어갈때
+            {
+                TestDialogueSystem.Instance.ShowDialogue(testDialogueData.GetDialogue("Chapter1","RightRoom")[1]);
+                GetInRightUpRoom++;
+            } 
+
+            if (CheckStairRoom == 1 || Input.GetKeyDown(KeyCode.Alpha4)) // 나와서 복도에 계단방을 확인할때
+            {
+                TestDialogueSystem.Instance.ShowDialogue(testDialogueData.GetDialogue("Chapter1","Corrider")[1]);
+                CheckStairRoom++;
+            }
+        }
+
+        private IEnumerator StartDialogue()
+        {
+            yield return WaitDelay(4f);
+            TestDialogueSystem.Instance.ShowDialogue(testDialogueData.GetDialogue("Chapter1","FirstRoom")[0]);
+        }
+
+        private IEnumerator WaitDelay(float delay)
+        {
+            yield return new WaitForSecondsRealtime(delay);
+        }
+    }
+}
+
