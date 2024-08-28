f1cc860fbd09eb599b2e2842632872eeb3793091
mandu <jys0972@gmail.com>
2024-08-28 16:52:37 +0900

    Create(Logger.cs)

---
+++ Assets/Util/DebugSystem/Scripts/Logger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class Logger : MonoBehaviour
+{
+}
