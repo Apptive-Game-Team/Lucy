3fdcb8f303e8e503ff458454a35922c7e36433a4
mandu <88422717+YunseongJeong@users.noreply.github.com>
2024-08-28 15:30:51 +0900

    85 어두운 곳에 있을 때 정신력이 깎이는 시스템 구현 (#91)
    * feat(Detector.cs) : ViewAngle Setter 구현
    
    * feat(Character_Stat.cs) : SpotLight 아래 있을 때 이벤트 구현
    
    * feat(Player.prefab) : 중복된 component 삭제
    
    * feat(SpotLight.prefab) : SpotLight의 prefab 구현
    
    * Create Spot Light.prefab.meta
    
    * feat(LightTestScene_ver2.unity) : 맨탈 깎김 이벤트 테스트용 씬 구현
    
    * feat(SpotLight.cs) : Dectector를 사용해서 Player를 인식하는 script 구현
    
    * refact(LightSystem_3F_Integrated.prefab) : SpotLight prefab을 사용하도록 변경

---
+++ Assets/LightSystem/Scripts/SpotLight.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLight : MonoBehaviour
+{
+    Detector detector;
+
+    List<Collider2D> targets = new List<Collider2D>();
+
+    private void Awake()
+    {
+        detector = gameObject.GetComponent<Detector>();
+    }
+
+    void Start()
+    {
+        detector.SetViewAngle(360);
+        detector.setLookingAngle(0);
+        detector.SetTargetMask(LayerMask.GetMask("Player"));
+        StartCoroutine(CheckLightUpdate());
+    }
+
+    IEnumerator CheckLightUpdate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            targets.Clear();
+            targets = detector.DetectByView();
+            foreach (Collider2D target in targets)
+            {
+                if (target.CompareTag("Player"))
+                {
+                    target.gameObject.GetComponent<Character_Stat>().OnSpotLight();
+                }
+            }
+        } 
+    }
+}
