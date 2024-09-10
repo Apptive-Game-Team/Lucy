910cfff1afc9704cceab8c1fffe8cb4286d7080a
mandu <jys0972@gmail.com>
2024-09-10 21:22:31 +0900

    feat(ActorSoundController.cs) : 달리거나 걸을 때 발자국 소리 재생 로직 구현

--- Assets/SoundSystem/Scripts/ActorSoundController.cs
+++ Assets/SoundSystem/Scripts/ActorSoundController.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SoundSystem;
 
 public class ActorSoundController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SoundManager soundManager;
+    private List<SoundSource> walkSounds;
+    private List<SoundSource> runSounds;
+    private AudioSource audioSource;
+    private Coroutine _footstepSoundCoroutine;
+    private int footstepSoundCount;
+    private bool _isRun;
+
+    private void Awake()
     {
-        
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        soundManager = SoundManager.Instance;
+        InitSoundLists();
+    }
+
+    private void InitSoundLists()
+    {
+        walkSounds = soundManager.soundSources.GetSoundSourcesByNameContain("Walk");
+        runSounds = soundManager.soundSources.GetSoundSourcesByNameContain("Run");
+        footstepSoundCount = Mathf.Min(walkSounds.Count, walkSounds.Count);
+    }
+
+    IEnumerator FootSoundPlay()
+    {
+        int footstepSoundIndex = 0;
+
+        while (true)
+        {
+            audioSource.clip = (_isRun ? runSounds : walkSounds)[footstepSoundIndex].sound;
+            audioSource.Play();
+
+            yield return new WaitWhile(() => audioSource.isPlaying);
+
+            footstepSoundIndex = (footstepSoundIndex + 1) % footstepSoundCount;
+        }
+    }
+
+    public void StartFootstepSoundPlay(bool isRun)
+    {
+        _isRun = isRun;
+        if (_footstepSoundCoroutine == null)
+        {
+            _footstepSoundCoroutine = StartCoroutine(FootSoundPlay());
+        }
+    }
+
+    public void StopFootstepSoundPlay()
+    {
+        if (_footstepSoundCoroutine != null)
+        {
+            StopCoroutine(_footstepSoundCoroutine);
+            _footstepSoundCoroutine = null;
+        }
     }
 }
