87bd11dcc69f25945aab1c74f1a86b4da852a634
mandu <jys0972@gmail.com>
2024-09-11 18:57:29 +0900

    feat(HandLightRotator.cs) : OnKey를 사용하도록 수정

--- Assets/LightSystem/Scripts/HandLightRotator.cs
+++ Assets/LightSystem/Scripts/HandLightRotator.cs
@@ -8,7 +8,7 @@ public abstract class HandLightComponent : MonoBehaviour
     public abstract void InitComponent();
 }
 
-public class HandLightRotator : MonoBehaviour
+public class HandLightRotator : MonoBehaviour, IKeyInputListener
 {
     [Tooltip("Please Find HandLight GameObject and Refer on it!")]
     [SerializeField] GameObject handlightObject;
@@ -22,17 +22,17 @@ public class HandLightRotator : MonoBehaviour
 
     private void Start()
     {
+        InputManager.Instance.SetKeyListener(this);
         if(handlightObject == null)
         {
             Debug.LogError("HandLight Object�� ���� �����ϴ�! ������ player ������ �ִ� ������Ʈ�� ã�Ƽ� �������� �ּ���!");
         }    
     }
-    
-    void Update()
+    void IKeyInputListener.OnKey(ActionCode action)
     {
-        Vector3 direction = InputManager.Instance.GetMoveVector();
-        if (!direction.Equals(Vector3.zero))
+        if ((int) action >= (int)ActionCode.MoveUp && (int) action <= (int)ActionCode.MoveLeft)
         {
+            Vector3 direction = InputManager.Instance.GetMoveVector();
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             handlightObject.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
             detector.SetLookingDirection(direction);
