a64bd3bfb1cb11bd523f582f6f7f36d70e9ada25
mandu <jys0972@gmail.com>
2024-09-11 17:08:47 +0900

    feat(SoundController.cs) : AudioSource 초기화 기능 구현

--- Assets/SoundSystem/Scripts/SoundController.cs
+++ Assets/SoundSystem/Scripts/SoundController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SoundController : MonoBehaviour
 {
+    protected AudioSource audioSource;
 
+    protected void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            throw new Exception("Audio Source is not found");
+        }
     }
 }
