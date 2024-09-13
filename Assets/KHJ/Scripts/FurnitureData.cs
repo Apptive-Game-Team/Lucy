8bf09ac6641c16b5058cc1ec6cde3cfa2accb587
191012 <kimhj191012@naver.com>
2024-09-13 20:24:35 +0900

    feat(FurnitureData.cs)
    가구 목록 ScriptableObject로 구현

---
+++ Assets/KHJ/Scripts/FurnitureData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FurnitureType
+{
+    Cabinet,
+    Bookshelf,
+    Drawer
+}
+
+[CreateAssetMenu(fileName = "Furniture", menuName = "New Furniture")]
+public class FurnitureData : ScriptableObject
+{
+    public string furnitureName;
+    public FurnitureType furnitureType;
+}
