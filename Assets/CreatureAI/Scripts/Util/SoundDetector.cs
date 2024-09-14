76a4ba60b398058c4c64f65f8526a3d64da53d7a
Yunseong Jeong <88422717+YunseongJeong@users.noreply.github.com>
2024-09-14 16:22:20 +0900

    Feature/118 (#130)
    * feat(TagManager.asset) : Layer에 Door 추가
    
    * feat(Doors) : Layer 설정
    
    * refact(Util.cs) : Array 연산 클래스 Util class로 수정
    
    * feat(Util.cs) : 중복 없이 하나의 List로 연결 시켜주는 함수 구현
    
    * feat(SoundDetector.cs) : Sound를 인식하는 class 구현
    
    * Update SoundSystemTestScene.unity
    
    * refact(CreatureManager.cs) : Util class를 사용하도록 수정
    
    * feat(Creature.cs) : Creature가 소리로도 Player를 쫒도록 수정
    
    * feat(SoundDetector.cs) : 로직 수정
    
    * Update SoundSystemTestScene.unity
    
    * refact(SoundDetector.cs) : 거리 수정
    
    * refact(Creature.cs) : delay 수정

---
+++ Assets/CreatureAI/Scripts/Util/SoundDetector.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SoundDetector : MonoBehaviour
+{
+    private float maxDistance = 10f;
+
+    [SerializeField] bool debugMode = true;
+
+    [SerializeField] LayerMask targetMask;
+
+    List<Collider2D> hitTargetList = new List<Collider2D>();
+
+    Vector3? targetPosition;
+
+    public void SetTargetMask(LayerMask targetMask)
+    {
+        this.targetMask = targetMask;
+    }
+
+    public List<Collider2D> Detect()
+    {
+        Collider2D[] targets = FindNearColliders();
+        hitTargetList.Clear();
+
+        foreach(Collider2D target in targets)
+        {
+            float distance = Vector3.Distance(transform.position, target.transform.position);
+            AudioSource source = target.GetComponent<AudioSource>();
+
+            if (distance < target.GetComponent<AudioSource>().maxDistance && source.isPlaying)
+            {
+                hitTargetList.Add(target);
+            }
+        }
+
+        if (debugMode)
+        {
+            print("sound detector found " + hitTargetList.Count + " target");
+        }
+        
+
+        return hitTargetList;
+    }
+
+    private Collider2D[] FindNearColliders()
+    {
+        Collider2D[] targets = Physics2D.OverlapCircleAll(
+                transform.position,
+                maxDistance,
+                targetMask
+            );
+
+        return targets;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (debugMode)
+        {
+            Gizmos.color = Color.red;
+            Vector3 myPos = transform.position;
+            Gizmos.DrawWireSphere(myPos, maxDistance);
+
+            if (targetPosition.HasValue)
+            {
+                Debug.DrawLine(
+                 transform.position,
+                 targetPosition.Value,
+                 Color.red
+                 );
+            }
+        }
+    }
+}
