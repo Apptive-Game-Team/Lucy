4f89c5217473fe5a8a28ac0dcd8e4bf72e4330a4
Gimlocal <127363458+Gimlocal@users.noreply.github.com>
2024-09-14 19:18:08 +0900

    120 이벤트 구현 및 기타사항 변경 (#143)
    * feat(Removed diagonal move)
    
    캐릭터의 대각선 이동 제한.
    이동 방향의 리스트를 만들어서 여러 키를 눌렀다 때도 방향을 기억해서 이동함.
    
    * fix(Remote Controller)
    
    리모컨 상호작용시 바로 배터리 획득 -> 리모컨 상호작용시 인벤토리로 들어오고 사용해야 배터리 획득으로 변경
    
    * Update Test_09.11.unity
    
    * feat(FurnitureData.cs)
    
    가구 목록 ScriptableObject로 구현
    
    * feat(Furniture.cs, FurnitureController.cs)
    
    추상화를 이용한 상호작용 구현(FurnitureController.cs)
    
    가구에 집어넣을 스크립트 구현(Furniture.cs)
    
    * Update Test_09.11.unity
    
    * feat(Hide in Cabinet)
    
    캐비넷이 숨는 기능 추가
    
    * Update Test_09.11.unity
    
    * feat(Furniture : Bookshelf)
    
    책장 구현 : 책장과 상호작용 시 이미지 나옴(이미지 미구현)
    
    * feat(Furniture : Drawer)
    
    열고 닫을 수 있는 서랍 + 열었을때 얻을수 있는 열쇠 구현
    
    * fix(Remove Dialogue Scripts)
    
    안쓰는 대사 스크립트 삭제
    
    * fix
    
    수정사항 적용
    
    * feat(NpcDialogue.)
    
    Npc와 상호작용 했을 때의 대사 구현

---
+++ Assets/KHJ/Scripts/NpcDialogueController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum NpcType
+{
+    puzzleNpc,
+}
+
+public abstract class NpcDialogues
+{
+    public abstract void Show();
+}
+
+public class puzzleNpc : NpcDialogues
+{
+    private NpcDialogueData npcDialogueData;
+    public puzzleNpc(NpcDialogueData npcDialogueData)
+    {
+        this.npcDialogueData = npcDialogueData;
+    }
+
+    public override void Show()
+    {
+        NpcDialogueController.Instance.ShowDialogue(npcDialogueData.GetDialogues(NpcType.puzzleNpc.ToString()));
+    }
+}
+
+public class NpcDialogueController : MonoBehaviour
+{
+    public static NpcDialogueController Instance { get; private set;}
+    public NpcDialogueData npcDialogueData;
+    protected List<NpcDialogues> npcDialogue;
+    public GameObject dialogueImage;
+    public GameObject dialogueCharacter;
+    public TextMeshProUGUI dialogueText;
+
+    public float typingSpeed = 0.05f;
+    public float delayBetweenDialogues = 1.2f;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        dialogueImage.SetActive(false);
+        dialogueCharacter.SetActive(false);
+    }
+
+    void Start()
+    {
+        npcDialogue = new List<NpcDialogues>()
+        {
+            new puzzleNpc(npcDialogueData)
+        };
+    }
+
+    public void ShowDialogue(string[] dialogues, float delayBetweenDialogues = 1.5f) // 두 문장 이상 출력
+    {
+        StartCoroutine(DisplayDialogues(dialogues, delayBetweenDialogues));
+    }
+
+    public void ShowDialogue(string dialogue) // 한 문장 출력
+    {
+        string[] singleDialogue = new string[] { dialogue };
+        ShowDialogue(singleDialogue);
+    }
+
+    private IEnumerator DisplayDialogues(string[] dialogues, float delayBetweenDialogues)
+    {
+        dialogueImage.SetActive(true);
+        dialogueCharacter.SetActive(true);
+        Time.timeScale = 0f;
+
+        foreach (string dialogue in dialogues)
+        {
+            yield return StartCoroutine(TypeDialogue(dialogue));
+            yield return new WaitForSecondsRealtime(delayBetweenDialogues);
+        }
+
+        dialogueImage.SetActive(false);
+        dialogueCharacter.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    private IEnumerator TypeDialogue(string dialogue)
+    {
+        dialogueText.text = "";
+
+        foreach (char letter in dialogue.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSecondsRealtime(typingSpeed);
+        }
+    }
+}
