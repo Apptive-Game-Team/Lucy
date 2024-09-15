601727d6762246d0d36f94aa2e4ed7fa95e806e4
mandu <jys0972@gmail.com>
2024-09-15 14:33:32 +0900

    feat(PortalManager.cs) : OnSceneChange 호출 추가

--- Assets/SceneTransition/Script/PortalManager.cs
+++ Assets/SceneTransition/Script/PortalManager.cs
@@ -8,9 +8,26 @@ public class PortalManager : SingletonObject<PortalManager>
     [SerializeField] SceneDataList sceneDataList;
     [SerializeField] PortalDataList portalDataList;
 
+    private List<ISceneChangeListener> listeners = new List<ISceneChangeListener>();
+
     private void Start()
     {
         DontDestroyOnLoad(this);
+        StartCoroutine(CallOnSceneChange());
+    }
+
+    public void SetSceneChangeListener(ISceneChangeListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    private IEnumerator CallOnSceneChange()
+    {
+        yield return new WaitForSeconds(0.1f);
+        foreach (ISceneChangeListener listener in listeners)
+        {
+            listener.OnSceneChange();
+        }
     }
 
     public void TransitScene(PortalID portalID)
@@ -18,6 +35,7 @@ public class PortalManager : SingletonObject<PortalManager>
         PortalData portalData = portalDataList.GetPortalDataByID(portalID);
         SceneData sceneData = sceneDataList.GetSceneDataByID(portalData.sceneID);
         SceneManager.LoadScene(sceneData.sceneName);
+        StartCoroutine(CallOnSceneChange());
         Character.Instance.transform.position = portalData.destination;
     }
 }
