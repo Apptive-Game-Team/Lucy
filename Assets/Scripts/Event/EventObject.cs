9b9b4fd2c7e83d458e3787074e68593bd2fe5fc9
mandu <jys0972@gmail.com>
2024-09-18 15:46:26 +0900

    feat(EventObject.cs) : 2층 이벤트 구체화

--- Assets/Scripts/Event/EventObject.cs
+++ Assets/Scripts/Event/EventObject.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventObject : MonoBehaviour
+public class EventObject : MonoBehaviour, ISceneChangeListener
 {
     private EventSoundController eventSoundController;
 
     void Start()
     {
+        PortalManager.Instance.SetSceneChangeListener(this);
         eventSoundController = GetComponent<EventSoundController>();
+        EventScheduler.Instance.eventObjects.Add("FirstMeetNpcEventObject", this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySound()
     {
-        
+        eventSoundController.PlaySound(SoundManager.Instance.soundSources.GetByName("OpenTheDoor2").Value.sound, true);
+    }
+    public void StopSound()
+    {
+        eventSoundController.StopSound();
+    }
+
+    public void OnSceneChange()
+    {
+        EventScheduler.Instance.UpdateGameEvent();
     }
 }
