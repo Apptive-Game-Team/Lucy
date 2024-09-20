5945268aa80d3b711e065de1797780cc2b403358
mandu <jys0972@gmail.com>
2024-09-20 19:16:25 +0900

    refact(Avoider.cs) : import 수정

--- Assets/Scripts/Scripts_Creatures/Creatures/Avoider.cs
+++ Assets/Scripts/Scripts_Creatures/Creatures/Avoider.cs
@@ -1,8 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
+using static Team6203.Util;
 
 namespace Creature
 {
