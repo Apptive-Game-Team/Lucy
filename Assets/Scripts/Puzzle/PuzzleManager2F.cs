b09dba3cbf1614439904c148dcb0a33857668002
Tonomi <163392234+hwanginseop@users.noreply.github.com>
2024-09-21 23:06:44 +0900

    Fix/175 (#184)
    * fix(0102.scenes)
    
    카메라 흔들림 수정, 중앙 계단 타일 삭제
    
    * fix(PuzzleManager2F.cs)
    
    카메라 스퀘어 수정중입니다
    
    * fix(PuzzleManager.cs)
    
    카메라 고정을 위한 스퀘어 이동을 구현했습니다. 근데 기능만 하고 겁나 짜쳐보임
    
    * fix(resolve conflict)
    
    resolve conflict

--- Assets/Scripts/Puzzle/PuzzleManager2F.cs
+++ Assets/Scripts/Puzzle/PuzzleManager2F.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class PuzzleManager2F : SingletonObject<PuzzleManager2F>, ISceneChangeListener
 {
     [SerializeField] BridgeDataList bridgeDataList;
     private List<GameObject> bridges;
     private float CallBridgeActiveDelay = 1f;
+    private GameObject square;
+    private GameObject player;
     Dictionary<BridgeID, bool> isOns = new Dictionary<BridgeID, bool>();
 
     void InitIsOns()
@@ -31,7 +36,8 @@ public class PuzzleManager2F : SingletonObject<PuzzleManager2F>, ISceneChangeLis
     void ISceneChangeListener.OnSceneChange()
     {
         bridges = new List<GameObject>(GameObject.FindGameObjectsWithTag("Bridge"));
-
+        square = GameObject.Find("Square");
+        player = GameObject.Find("Player");
         foreach (GameObject bridge in bridges)
         {
             Bridge_Puzzle2F bridgeComponent = bridge.GetComponent<Bridge_Puzzle2F>();
@@ -39,6 +45,14 @@ public class PuzzleManager2F : SingletonObject<PuzzleManager2F>, ISceneChangeLis
             {
                 bridge.SetActive(isOns[bridgeComponent.bridgeID]);
             }
+        }
+        if(player.transform.position.x > -40f && SceneManager.GetActiveScene().name == "Puzzle_2F")
+        {
+            square.transform.position = new Vector3(12.06f, 0.13f, 0f);
+        }
+        else if(player.transform.position.x <= -40f && SceneManager.GetActiveScene().name == "Puzzle_2F")
+        {
+            square.transform.position = new Vector3(-92.0f, 0.13f, 0f);
         }
     }
 
