48c82adec5646bccb779a3639f60a77156c1cdcd
hwanginseop <his001015@gmail.com>
2024-09-21 22:44:10 +0900

    fix(PuzzleManager.cs)
    카메라 고정을 위한 스퀘어 이동을 구현했습니다. 근데 기능만 하고 겁나 짜쳐보임

--- Assets/Scripts/Puzzle/PuzzleManager2F.cs
+++ Assets/Scripts/Puzzle/PuzzleManager2F.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class PuzzleManager2F : SingletonObject<PuzzleManager2F>, ISceneChangeListener
 {
     [SerializeField] BridgeDataList bridgeDataList;
     private List<GameObject> bridges;
     private float CallBridgeActiveDelay = 1f;
     private GameObject square;
+    private GameObject player;
     Dictionary<BridgeID, bool> isOns = new Dictionary<BridgeID, bool>();
 
     void InitIsOns()
@@ -27,13 +31,13 @@ public class PuzzleManager2F : SingletonObject<PuzzleManager2F>, ISceneChangeLis
     private void Start()
     {
         PortalManager.Instance.SetSceneChangeListener(this);
-        square = GameObject.Find("CameraSquare");
     }
     // Start is called before the first frame update
     void ISceneChangeListener.OnSceneChange()
     {
         bridges = new List<GameObject>(GameObject.FindGameObjectsWithTag("Bridge"));
-
+        square = GameObject.Find("Square");
+        player = GameObject.Find("Player");
         foreach (GameObject bridge in bridges)
         {
             Bridge_Puzzle2F bridgeComponent = bridge.GetComponent<Bridge_Puzzle2F>();
@@ -42,7 +46,14 @@ public class PuzzleManager2F : SingletonObject<PuzzleManager2F>, ISceneChangeLis
                 bridge.SetActive(isOns[bridgeComponent.bridgeID]);
             }
         }
-        square.transform.position = new Vector3(12.1f, -0.1f, 0f);
+        if(player.transform.position.x > -40f && SceneManager.GetActiveScene().name == "Puzzle_2F")
+        {
+            square.transform.position = new Vector3(12.06f, 0.13f, 0f);
+        }
+        else if(player.transform.position.x <= -40f && SceneManager.GetActiveScene().name == "Puzzle_2F")
+        {
+            square.transform.position = new Vector3(-92.0f, 0.13f, 0f);
+        }
     }
 
     private IEnumerator CallActivateBridge(BridgeID bridgeID)
