cbe7c90ce4ab29f56abb13c6f6992ccceee23219
hwanginseop <163392234+hwanginseop@users.noreply.github.com>
2024-09-22 13:01:19 +0900

    feat(NpcEventController)
    페이드 인 아웃 방식 변경

--- Assets/Scripts/Event/NpcEventController.cs
+++ Assets/Scripts/Event/NpcEventController.cs
@@ -11,8 +11,6 @@ public class NpcEventController : MonoBehaviour
     private Guard guard;
     private GameObject player;
     private GameObject barricade;
-    private GameObject blackOutImage;
-    private SpriteRenderer blackOutImageSpriteRenderer;
     private CharacterMove playerMoveScript;
     private float npcEventTime = 5f;
     private float blackOutDelay = 3f;
@@ -26,12 +24,7 @@ public class NpcEventController : MonoBehaviour
 
         barricade = GameObject.Find("Barricade");
 
-        blackOutImage = GameObject.Find("blackOut");
-        blackOutImageSpriteRenderer = blackOutImage.GetComponent<SpriteRenderer>();
-
         Npc = GameObject.Find("NPC");
-
-        SetBlackOutImageAlpha(0);
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -51,27 +44,12 @@ public class NpcEventController : MonoBehaviour
         StartCoroutine(WaitAndFinishNpcEvent());
     }
 
-    private IEnumerator BlackOutEffect()
-    {
-        for (float alpha = 0; alpha <= 1; alpha += Time.deltaTime / blackOutDelay)
-        {
-            SetBlackOutImageAlpha(alpha);
-            yield return null;
-        }
-        yield return new WaitForSeconds(blackOutDelay);
-        Destroy(Npc);
-        for (float alpha = 1; alpha >= 0; alpha -= Time.deltaTime / blackOutDelay)
-        {
-            SetBlackOutImageAlpha(alpha);
-            yield return null;
-        }
-        FinishNpcEvent();
-    }
-
     private IEnumerator WaitAndFinishNpcEvent()
     {
         yield return new WaitForSeconds(npcEventTime);
-        StartCoroutine(BlackOutEffect());
+        yield return CameraEffector.Instance.FadeOut();
+        Destroy(Npc);
+        StartCoroutine(CameraEffector.Instance.FadeIn());
     }
 
     private void FinishNpcEvent()
@@ -79,12 +57,5 @@ public class NpcEventController : MonoBehaviour
         playerMoveScript.ResumeMovement();
         guard.StartPatrol();
         Destroy(this);
-    }
-
-    private void SetBlackOutImageAlpha(float alpha)
-    {
-        Color color = blackOutImageSpriteRenderer.color;
-        color.a = alpha;
-        blackOutImageSpriteRenderer.color = color;
     }
 }
